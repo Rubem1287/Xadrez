d2c85a1ed03aea5a9e9b3e7f42d2fc809ca38445
Rubem Silva <rubem.edu@outlook.com>
2021-05-03 21:42:48 -0300

    Adicionar arquivos de projeto.

---
+++ Xadrez/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xadrez.Tabuleiro;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
